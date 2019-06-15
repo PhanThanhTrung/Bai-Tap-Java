cdabbdd62b115eb6c60c70a1bfc7e57abc23f1bf
PhanThanhTrung <trung99nb@gmail.com>
2019-06-15 10:06:51 +0700

    them bai 16 bai tap thuc hanh java

---
+++ bai16/bai16.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bai16;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Phan Thanh Trung
+ */
+public class bai16 {
+    public static void main(String[] args) {
+        Scanner s= new Scanner(System.in);
+        String str= s.nextLine();
+        int n = s.nextInt();
+        for(int i=0; i<n; i++)
+        {
+            System.out.print(str);
+        }
+    }
+}
